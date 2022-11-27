63dae40e6be168ce5c31588cdd28d099f45b405d
Florian Schmaus <flo@geekplace.eu>
2022-11-27 19:03:44 +0100

    WIP: scala

--- beanstalk-app/src/main/java/eu/geekplace/beanstalk/app/App.java
+++ beanstalk-app/src/main/java/eu/geekplace/beanstalk/app/App.java
@@ -3,5 +3,7 @@ package eu.geekplace.beanstalk.app;
 public class App {
     public static void main(String[] args) throws InterruptedException {
     	eu.geekplace.beanstalk.core.loom.MinimalStructuredTaskScope.run();
+    	var test = new eu.geekplace.beanstalk.core.scala.BeanstalkScala();
+    	test.sayHello();
     }
 }
---
+++ beanstalk-core/src/main/scala/eu/geekplace/beanstalk/core/scala/BeanstalkScala.scala
@@ -0,0 +1,11 @@
+package eu.geekplace.beanstalk.core.scala
+
+class BeanstalkScala {
+  def sayHello() = println("Hello World from Scala")
+}
+
+object BeanstalkScala {
+
+  def apply() = println("Hello World from Scala")
+
+}
