7d6e669271255b61e17ed30a1b638f5932722916
Florian Schmaus <flo@geekplace.eu>
2023-01-30 14:13:23 +0100

    Add NowaTest

---
+++ beanstalk-core/src/test/java/eu/geekplace/beanstalk/core/loom/nowa/NowaTest.java
@@ -0,0 +1,15 @@
+package eu.geekplace.beanstalk.core.loom.nowa;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class NowaTest {
+
+	@Test
+	public void fib() throws InterruptedException {
+		long res = NowaExamples.fib(16);
+		assertEquals(42L, res);
+	}
+	
+}
