340c17d096558458e6dadb94d517c59462a48f1f
Florian Schmaus <flo@geekplace.eu>
2023-01-30 14:13:13 +0100

    Invoke NowaExamples.fib() in App

--- beanstalk-app/src/main/java/eu/geekplace/beanstalk/app/App.java
+++ beanstalk-app/src/main/java/eu/geekplace/beanstalk/app/App.java
@@ -1,11 +1,16 @@
 package eu.geekplace.beanstalk.app;
 
+import eu.geekplace.beanstalk.core.loom.nowa.NowaExamples;
+
 public class App {
 
 	public static void main(String[] args) throws InterruptedException {
 		eu.geekplace.beanstalk.core.loom.MinimalStructuredTaskScope.run();
 		var test = new eu.geekplace.beanstalk.core.scala.BeanstalkScala();
 		test.sayHello();
+
+		long fibSixteen = NowaExamples.fib(16);
+		System.out.println("fib(16): " + fibSixteen);
 	}
 
 }
