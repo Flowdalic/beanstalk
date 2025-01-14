fe07838e5d508ab089af7f1fa62c3c96274af2d4
Florian Schmaus <flo@geekplace.eu>
2025-01-14 16:31:36 +0100

    Bump to gradle 8.12 and Java 21

--- beanstalk-core/src/main/java/module-info.java
+++ beanstalk-core/src/main/java/module-info.java
@@ -4,6 +4,5 @@ module eu.geekplace.beanstalk.core {
 	exports eu.geekplace.beanstalk.core.loom;
 	exports eu.geekplace.beanstalk.core.scala;
 
-	requires jdk.incubator.concurrent;
 	requires org.scala.lang.scala3.library;
 }
